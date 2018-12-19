61c38c6de7850ff43e10acaa148f3357b2513afb
Mike Landers <landers.mike@gmail.com>
2018-12-19 13:55:38 -0800

    Inital attempt at rebuilding in react

--- src/index.js
+++ src/index.js
@@ -6,28 +6,28 @@ import "./styles.css";
 function App() {
   return (
     <React.Fragment>
-          <div class="container">
-        <div class="headerWrap">
+          <div className="container">
+            <div className="headerWrap">
             <header>
-                <a href="index.html" class="logo">Cranders</a>
+                <a href="index.html" className="logo">Cranders</a>
                 <nav>
                     <a href="index.html">Home</a>
-                    <!-- <a href="#">About</a>
-                    <a href="#portfolio">Portfolio</a> -->
+                    <a href="#">About</a>
+                    <a href="#portfolio">Portfolio</a>
                     <a href="#">Contact</a>
                 </nav>
             </header>
         </div>
-        <div class="hero"><div class="attribution">Credit: <a href="https://www.flickr.com/photos/98786299@N00/9889667656/">Flickr/Peter Thoeny</a></div>
-            <div class="tagline"><h1>Full Stack Web Developer</h1><br><h4>In Training</h4></div>
-            <div class="headshot"><img src="img/headshot.png" alt=""></div>
-        </div>
-        <div class="bodyWrap">
-            <div class="portfolio">
+              <div className="hero"><div className="attribution">Credit: <a href="https://www.flickr.com/photos/98786299@N00/9889667656/">Flickr/Peter Thoeny</a></div>
+                  <div className="tagline"><h1>Full Stack Web Developer</h1><br/><h4>In Training</h4></div>
+                  <div className="headshot"><img src="./img/headshot.png" alt="" /></div>
+              </div>
+            <div className="bodyWrap">
+            <div className="portfolio">
                 <h2>Projects</h2>
             </div>
         </div>
-        <footer>
+            <footer>
             <nav>
                 <a href="#">Home</a>
                 <a href="#">Contact</a>
