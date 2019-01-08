3bcb07c01c789cfaaa790b194c67ff7ed84a84be
Michael Landers <landers.mike@gmail.com>
2019-01-07 20:35:42 -0800

    created header

---
+++ src/components/Header/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Menu() {
+    return (
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="www.apple.com">About</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="www.apple.com">Contact</a>
+        </nav>
+
+    )
+}
+
+export default Menu
