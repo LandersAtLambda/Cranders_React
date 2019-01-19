d1a38436e2b40565c8b27ed6d84e3c6a4cd778aa
Mike Landers <landers.mike@gmail.com>
2019-01-19 00:03:11 -0800

    merge?

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './components/App'
-
-import "./styles.css";
-
-
-const rootElement = document.getElementById("root");
->>>>>>> 3bcb07c01c789cfaaa790b194c67ff7ed84a84be
 ReactDOM.render(<App />, rootElement);
