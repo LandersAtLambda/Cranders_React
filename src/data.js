2441f70c3817e775fa5db6dd729547618b7a0954
Michael Landers <landers.mike@gmail.com>
2019-03-04 22:19:51 -0800

    correct screenshot images for projects

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ const projects = [
 		id: 2,
 		name: 'Instagram Clone',
 		img:
-			'https://github.com/mlanders/Cranders_React/blob/master/src/img/apps/Project%202.png?raw=true',
+			'https://github.com/mlanders/Cranders_React/blob/master/src/img/apps/Project%203.png?raw=true',
 		github: 'https://github.com/mlanders/React-Insta-Clone',
 		preview: 'https://instagram.cranders.com/',
 		tags: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-node-js']
@@ -21,7 +21,7 @@ const projects = [
 		id: 3,
 		name: 'React Wars',
 		img:
-			'https://github.com/mlanders/Cranders_React/blob/master/src/img/apps/Project%203.png?raw=true',
+			'https://github.com/mlanders/Cranders_React/blob/master/src/img/apps/Project%202.png?raw=true',
 		github: 'https://github.com/mlanders/Sprint-Challenge-React-Wars',
 		preview: 'https://reactwars.cranders.com/',
 		tags: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-node-js']
